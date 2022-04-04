7fea22eff811069d36828928c9ec52fafb338917
ienva <ienriqva@mercadona.es>
2022-04-04 11:10:45 +0200

    first refactor

--- main.py
+++ main.py
@@ -2,27 +2,26 @@
 
 class Character:
     def __init__(self):
+        self.max_health = 1000
+        self.min_health = 0
         self.health = 1000
         self.level = 1
         self.alive = True
 
     def received_damage(self, amount_of_damage):
         self.health -= amount_of_damage
-        if self.health < 0:
-            self.health = 0
+        if self.health < self.min_health:
+            self.health = self.min_health
 
         self.alive = self.is_alive()
 
     def is_alive(self):
-        if self.health > 0:
-            return True
-        else:
-            return False
+        return self.health > self.min_health
 
     def heal(self, amount_of_heal):
         if self.alive is False:
             raise ValueError("Character already dead")
         else:
             self.health += amount_of_heal
-            if self.health > 1000:
-                self.health = 1000
+            if self.health > self.max_health:
+                self.health = self.max_health
